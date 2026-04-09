00e677c8b335d2eb3472834026d5c434d4353da4
yahir <s22120159@alumnos.itsur.edu.mx>
2026-04-09 15:34:45 -0600

    cambios

--- app/src/main/java/com/example/ubicacion/RouteService.kt
+++ app/src/main/java/com/example/ubicacion/RouteService.kt
@@ -1,2 +1,20 @@
 package com.example.ubicacion
 
+import android.telecom.Call
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RouteService {
+    @GET("v2/directions/driving-car")
+    fun getRoute(
+        @Query("api_key") apiKey: String,
+        @Query("start") start: String, // format: "lng,lat"
+        @Query("end") end: String     // format: "lng,lat"
+    ): Call<RouteResponse>
+}
+
+// Clase para parsear la respuesta JSON (Simplificada)
+data class RouteResponse(val features: List<Feature>)
+data class Feature(val geometry: Geometry)
+data class Geometry(val coordinates: List<List<Double>>)
