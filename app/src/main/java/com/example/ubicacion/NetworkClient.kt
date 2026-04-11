c354c8fc3f1f5fc69f6a12d6f8991241304680f6
yahir <s22120159@alumnos.itsur.edu.mx>
2026-04-11 16:20:59 -0600

    cambios

--- app/src/main/java/com/example/ubicacion/NetworkClient.kt
+++ app/src/main/java/com/example/ubicacion/NetworkClient.kt
@@ -9,14 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object NetworkClient {
 
-    // URL base de la API de OpenRouteService para obtener rutas
     private const val BASE_URL = "https://api.openrouteservice.org/"
 
-    // Inicialización perezosa (lazy) de Retrofit
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            // GsonConverterFactory convierte el JSON de la API automáticamente a nuestras clases de datos
+
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
